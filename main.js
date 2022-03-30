6237b051fbba26000cfe907f22878b430ab1428a
NikunjMehta302010 <76863868+NikunjMehta302010@users.noreply.github.com>
2022-03-30 21:34:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+status = "";
+objects= [];
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+}
+
+function start() {
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    object_name = document.getElementById("object_name").value;
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+}
+
